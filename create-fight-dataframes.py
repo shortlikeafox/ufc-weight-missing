0fd4484cdede33a710a69f566d6fda534f1eb037
shortlikeafox <62516729+shortlikeafox@users.noreply.github.com>
2020-04-14 14:04:58 -0500

    made fight dataframes

---
+++ create-fight-dataframes.py
@@ -0,0 +1,52 @@
+from os import listdir
+from os.path import isfile, join
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+my_path = 'fight-page-table-dump/'
+
+files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+
+#for file in files:
+#    print(file)
+for ff in files:
+    fight_file=open(my_path + ff, "r")
+    
+    
+    bs=BeautifulSoup(fight_file.read(), 'html.parser')
+    
+    #print(files[0])
+    
+    tdList = bs.find_all('td')
+    i = 0
+    
+    winners = []
+    losers = []
+    for td in tdList:
+        #print(f"{i}, {td}")
+        if i%8 == 1:
+            winner=td.get_text()
+            winner=winner.rstrip("n")
+            winner=winner.rstrip("\\")
+            #print(winner)
+            winners.append(winner)
+        if i%8 == 3:
+            loser=td.get_text()
+            loser=loser.rstrip("n")
+            loser=loser.rstrip("\\")
+            losers.append(loser)
+        i=i+1
+        
+        
+    df = pd.DataFrame(list(zip(winners, losers)), columns=['winner', "loser"])
+    
+    display(df)
+    
+    #Strip the table part
+    event_name = ff[:-6]
+    
+    df["event"] = event_name
+    display(df)
+    
+    df.to_csv(f'fight-dataframes/{event_name}.csv')
